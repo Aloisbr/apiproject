f7266724d4c855c041b5e8104571f9bb7068b40a
Aloïs <a.beurey@it-students.fr>
2021-02-18 10:16:17 +0100

    carousel

--- js/artist.js
+++ js/artist.js
@@ -1,4 +1,22 @@
-let list= document.querySelector('#sidebar');
+let list = document.querySelector('#sidebar');
+
+function sortInTab (tab) {
+    let i = 0;
+    let s = 0;
+    while (i < tab.length - 1)
+    {
+        if (tab[i].vote_count < tab[i + 1].vote_count)
+        {
+            s = tab[i];
+            tab[i] = tab[i + 1];
+            tab[i + 1] = s;
+            i = 0;
+        }
+        else 
+            i++;
+    }
+    return tab;
+}
 
 function getList () {
     fetch('https://api.themoviedb.org/3/person/popular?api_key=01af314f34b767f06d2445d9fc982dd9')
@@ -27,7 +45,6 @@ list.addEventListener('click', (el) => {
             return response.json();
         })
         .then((data) => {
-            console.log(data);
             document.querySelector('#profile').src = "https://image.tmdb.org/t/p/w200" + data.profile_path;
             if(data.gender == 2)
                 document.querySelector("#gender").innerHTML = "Homme";
@@ -39,5 +56,49 @@ list.addEventListener('click', (el) => {
             document.querySelector("#biography").innerHTML = data.biography;
             document.querySelector("#main").classList.replace('none', 'flex');
         });
+        fetch('https://api.themoviedb.org/3/person/' + el.dataset.id + '/movie_credits?api_key=01af314f34b767f06d2445d9fc982dd9')
+        .then((response) => {
+            return response.json();
+        })
+        .then((data) => {
+            movies = sortInTab(data.cast).slice(0, 12);
+            movies.forEach(movie => {
+                let item = document.createElement('div');
+                let item_img = document.createElement('div');
+                let img = document.createElement('img');
+                let item_body = document.createElement('div');
+                let item_title = document.createElement('div');
+                let item_description = document.createElement('div');
+                let title = document.createElement('h3');
+                let description = document.createElement('p');
+                
+                img.src = "https://image.tmdb.org/t/p/w200" + movie.poster_path + '?api_key=01af314f34b767f06d2445d9fc982dd9';
+                title.innerHTML = movie.title;
+                description.innerHTML = movie.release_date;
+                item.classList.add("item");
+                item_img.classList.add("item__img");
+                item_body.classList.add("item__body");
+                item_title.classList.add("item__title");
+                item_description.classList.add("item__description");
+
+                item_description.appendChild(description);
+                item_title.appendChild(title);
+                item_img.appendChild(img);
+                item_body.appendChild(item_title);
+                item_body.appendChild(item_description);
+                item.appendChild(item_img);
+                item.appendChild(item_body);
+                document.querySelector('#carousel1').appendChild(item);
+            });
+            if(document.querySelector('.carousel'))
+                document.querySelector('.carousel').innerHTML = '';  
+            new Carousel(document.querySelector('#carousel1'), {
+            slidesToScroll: 2,
+            slidesVisible: 3,
+            loop: false
+            })
+
+        })
     }
 })
+
---
+++ js/carousel.js
@@ -0,0 +1,107 @@
+class Carousel {
+    constructor (element, options = {}) {
+        this.element = element;
+        this.options = Object.assign({}, {
+            slidesToScroll: 1,
+            slidesVisible: 1,
+            loop: false
+        }, options);
+        let children = [].slice.call(element.children);
+        this.isMobile = true;
+        this.currentItem = 0;
+        this.root = this.createDivWithClass('carousel');
+        this.container = this.createDivWithClass('carousel__container');
+        this.root.appendChild(this.container);
+        this.element.appendChild(this.root);
+        this.moveCallbacks = [];
+        this.items = children.map((child) => {
+            let item = this.createDivWithClass('carousel__item');
+            item.appendChild(child);
+            this.container.appendChild(item);
+            return item;
+        });
+        this.setStyle();
+        this.createNavigation();
+        this.moveCallbacks.forEach(cb => cb(0));
+        this.onWindowResize();
+        window.addEventListener('resize', this.onWindowResize.bind(this))
+    }
+
+    setStyle() {
+        let ratio = this.items.length / this.slidesVisible;
+        this.container.style.width = (ratio * 100) + "%";
+        this.items.forEach(item => item.style.width = ((100 / (this.slidesVisible)) / ratio) + '%');
+    }
+
+    createNavigation () {
+        let nextButton = this.createDivWithClass('carousel__next');
+        let prevButton = this.createDivWithClass('carousel__prev');
+        this.root.appendChild(nextButton);
+        this.root.appendChild(prevButton);
+        nextButton.addEventListener('click', this.next.bind(this));
+        prevButton.addEventListener('click', this.prev.bind(this));
+        if (this.options.loop === true) {
+            return;
+        }
+        this.onMove(index => {
+            if (index === 0) {
+                prevButton.classList.add('carousel__prev--hidden');
+            } else {
+                prevButton.classList.remove('carousel__prev--hidden');
+            }
+            if (this.items[this.currentItem + this.slidesVisible] === undefined) {
+                nextButton.classList.add('carousel__next--hidden');
+            } else {
+                nextButton.classList.remove('carousel__next--hidden');
+            }
+        })
+    }
+
+    next() {
+        this.gotoItem(this.currentItem + this.options.slidesToScroll);
+    }
+
+    prev() {
+        this.gotoItem(this.currentItem - this.options.slidesToScroll);
+    }
+
+    gotoItem(index) {
+        if (index < 0) {
+            index = this.items.length - this.options.slidesVisible;
+        } else if (index >= this.items.length || (this.items[this.currentItem + this.options.slidesVisible] === undefined) && index > this.currentItem) {
+            index = 0;
+        }
+        let translateX = index * -100 / this.items.length;
+        this.container.style.transform = 'translate3d(' + translateX +'%, 0, 0)';
+        this.currentItem = index;
+        this.moveCallbacks.forEach(cb => cb(index));
+    }
+
+    onMove(cb) {
+        this.moveCallbacks.push(cb);
+    }
+
+    onWindowResize () {
+        let mobile = window.innerWidth < 800;
+        if (mobile !== this.isMobile) {
+            this.isMobile = mobile;
+            this.setStyle();
+            this.moveCallbacks.forEach(cb => cb(this.currentItem));
+            
+        }
+    }
+
+    createDivWithClass(className) {
+        let div = document.createElement('div');
+        div.setAttribute('class', className);
+        return div;
+    }
+
+    get slidesToScroll () {
+        return this.isMobile ? 1 : this.options.slidesToScroll;
+    }
+
+    get slidesVisible() {
+        return this.isMobile ? 1 : this.options.slidesVisible;
+    }
+}
