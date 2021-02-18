82f3a3f11da21642f69652d0216b5ee1b5760bd1
Aloïs <a.beurey@it-students.fr>
2021-02-18 15:34:06 +0100

    menu burger update

---
+++ js/burger.js
@@ -0,0 +1,50 @@
+// Récupére les blocs que je veux afficher
+var mainMenu = document.querySelector("#menu");
+var burgerMenu = document.querySelector("#menu-burger");
+
+/*=== Clic sur le menu burger 
+/*===============================*/
+// Vérifie si l'événement touchstart existe et est le premier déclenché
+var clickedEvent = "click"; // Au clic si "touchstart" n'est pas détecté
+window.addEventListener(
+  "touchstart",
+  function detectTouch() {
+    clickedEvent = "touchstart"; // Transforme l'événement en "touchstart"
+    window.removeEventListener("touchstart", detectTouch, false);
+  },
+  false
+);
+
+// Créé un "toggle class" en Javascrit natif (compatible partout)
+burgerMenu.addEventListener(
+  clickedEvent,
+  function (evt) {
+    console.log(clickedEvent);
+    // Modification du menu burger
+    if (!this.getAttribute("class")) {
+      this.setAttribute("class", "clicked");
+    } else {
+      this.removeAttribute("class");
+    }
+
+    // Créé l'effet pour le menu slide (compatible partout)
+    if (mainMenu.getAttribute("class") != "visible") {
+      mainMenu.setAttribute("class", "visible");
+    } else {
+      mainMenu.setAttribute("class", "invisible");
+    }
+  },
+  false
+);
+
+window.addEventListener('resize', () => {
+    let burger = document.querySelector('#group')
+    let menu = document.querySelector("#header")
+    if (window.innerWidth < 1100) {
+        burger.style.display = '';
+        menu.style.display = 'none';
+    } else {
+        burger.style.display = 'none';
+        menu.style.display = '';
+    }
+});
